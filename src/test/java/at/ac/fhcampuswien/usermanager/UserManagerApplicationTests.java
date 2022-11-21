68bd24dadddc01b521f7a954aeee97bd83fbcfae
Eren Dogan <eren51@live.at>
2022-11-21 19:29:22 +0100

    Changes by leobw

--- src/test/java/at/ac/fhcampuswien/usermanager/UserManagerApplicationTests.java
+++ src/test/java/at/ac/fhcampuswien/usermanager/UserManagerApplicationTests.java
@@ -2,12 +2,16 @@ package at.ac.fhcampuswien.usermanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(locations={"classpath:WEB-INF/application-context.xml"})
 class UserManagerApplicationTests {
 
     @Test
     void contextLoads() {
+        //TODO fix test
+        assert true;
     }
 
 }
