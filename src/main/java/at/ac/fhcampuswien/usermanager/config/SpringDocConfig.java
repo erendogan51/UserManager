17aa4fad039c2efa67098120959d5bbf19648539
Leo Bowen <leobwang@gmail.com>
2022-11-21 20:00:47 +0100

    springdoc instead of springfox due to deprecation

---
+++ src/main/java/at/ac/fhcampuswien/usermanager/config/SpringDocConfig.java
@@ -0,0 +1,15 @@
+package at.ac.fhcampuswien.usermanager.config;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class SpringDocConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("usermanager")
+                .pathsToMatch("/public/**")
+                .build();
+    }
+}
