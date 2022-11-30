550314390170a55241d3aacd31aaaa4b4f703a7e
Leo Bowen <leobwang@gmail.com>
2022-11-30 18:53:33 +0100

    disable security login

--- src/main/java/at/ac/fhcampuswien/usermanager/service/UserService.java
+++ src/main/java/at/ac/fhcampuswien/usermanager/service/UserService.java
@@ -9,7 +9,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import usermanager.v1.model.NewUser;
-import org.springframework.security.crypto.
 
 @Service
 public class UserService {
