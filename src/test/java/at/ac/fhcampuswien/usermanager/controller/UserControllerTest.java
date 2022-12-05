adc4dd4a74e51f8a274f8e5276be0e3c4e8d7c43
Leo Bowen <leobwang@gmail.com>
2022-12-05 18:43:38 +0100

    remove junit due spring-boot-starter-test

---
+++ src/test/java/at/ac/fhcampuswien/usermanager/controller/UserControllerTest.java
@@ -0,0 +1,31 @@
+package at.ac.fhcampuswien.usermanager.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class UserControllerTest {
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void getUserByName() {
+    }
+
+    @Test
+    void loginUser() {
+    }
+
+    @Test
+    void logoutUser() {
+    }
+
+    @Test
+    void updatePassword() {
+    }
+}
