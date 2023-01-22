682df112645bc4fb487774e3734c2e5bbb2d02bd
Leo Bowen <leobwang@gmail.com>
2023-01-22 17:46:39 +0100

    add test coverage

--- src/test/java/at/ac/fhcampuswien/usermanager/service/UserServiceTest.java
+++ src/test/java/at/ac/fhcampuswien/usermanager/service/UserServiceTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import at.ac.fhcampuswien.usermanager.entity.UserEntity;
 import at.ac.fhcampuswien.usermanager.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -21,6 +22,7 @@ class UserServiceTest extends ServiceTestConfig{
     }
 
     @Test
+    @DisplayName("Save User correctly")
     void test_saveUser() {
         UserEntity given = new UserEntity();
         given.setUsername("test");
